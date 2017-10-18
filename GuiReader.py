c716684a5a54989f3d965ee3824997592f3c6c2e
jillzhouzh <32000082+jillzhouzh@users.noreply.github.com>
2017-10-17 23:46:47 -0500

    change format
    long line with Question

--- GuiReader.py
+++ GuiReader.py
@@ -1,44 +1,54 @@
 
 import tkinter
-from tkinter import Entry
+from tkinter import Tk, Label, Entry, StringVar
+'''
+# remnant of entry class I should finish
+class EntryFormat:
+    def __init__(self,labelTxt):
+        
+        self.label = Label(master, textvariable=labelTxt, height=4)
+        self.label.pack(side="left")
+
+        self.nameBox = Entry(master)
+        self.nameBox.pack(side="left")
+        self.Value = self.nameBox.get()
+'''
+
 # Code to add widgets will go here...
 master = tkinter.Tk()
 # add entry box for name
+nameText=StringVar()
+nameText.set("Enter directory of log files")
+nameLabel=Label(master, textvariable=nameText, height=4)
+nameLabel.pack(side="left")
+
 nameBox = Entry(master)
-nameBox.pack()
+nameBox.pack(side="left")
 nameVal = nameBox.get()
 # add entry box for sleepTime
+sleepText=StringVar()
+sleepText.set("On average, what time do you go to sleep? ")
+sleepLabel=Label(master, textvariable=sleepText, height=4)
+sleepLabel.pack(side="left")
+
 sleepBox = Entry(master)
-sleepBox.pack()
-sleepVal = sleepBox.get()
+sleepBox.pack(side="left")
 # add entry box for wakeTime
+wakeText=StringVar()
+wakeText.set("On average, what time do you wake up?")
+wakeLabel=Label(master, textvariable=wakeText, height=4)
+wakeLabel.pack(side="left")
+
 wakeBox = Entry(master)
-wakeBox.pack()
-wakeVal = wakeBox.get()
+wakeBox.pack(side="left")
 # add entry box for smoke
+smokeText=StringVar()
+smokeText.set("Are you okay with somking? \n(Y or N): ")
+smokeLabel=Label(master, textvariable=smokeText, height=4)
+smokeLabel.pack(side="left")
+
 smokeBox = Entry(master)
-smokeBox.pack()
-smokeVal = smokeBox.get()
-# add entry box for gender
-genderBox = Entry(master)
-genderBox.pack()
-genderVal = genderBox.get()
-# add entry box for gPrefer
-gPreferBox = Entry(master)
-gPreferBox.pack()
-gPrefer = gPreferBox.get()
-# add entry box for cleaniess
-cleanBox = Entry(master)
-cleanBox.pack()
-cleanVal = cleanBox.get()
-# add entry box for guest
-GuestBox = Entry(master)
-GuestBox.pack()
-GuestVal = GuestBox.get()
-# add entry box for sound
-soundBox = Entry(master)
-soundBox.pack()
-soundVal = soundBox.get()
+smokeBox.pack(side="left")
 
 master.mainloop()
 
