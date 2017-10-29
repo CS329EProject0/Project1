e98e1944447add593b52e0fc8ce54d296cd5ae0e
nmosley3 <31999154+nmosley3@users.noreply.github.com>
2017-10-29 16:25:56 -0500

    Final Release Ready

--- GuiReader.py
+++ GuiReader.py
@@ -7,10 +7,10 @@ class GUI():
 	def __init__(self, master):
 		self.master = master
 
-		self.master.title('Please answer the following questions.')
+		self.master.title('The Roommate Guru')
 
 		# scroll bar magic, don't touch
-		sizex = 540
+		sizex = 440
 		sizey = 500
 		posx  = 100
 		posy  = 100
@@ -28,37 +28,40 @@ class GUI():
 		self.canvas.pack(side="left")
 		self.canvas.create_window((0,0),window=frame,anchor='nw')
 		frame.bind("<Configure>", self.myfunction)
-
 		# complete scrollbar magic
 
 		# Code to add widgets 
 		# add entry box for first name
-		Label(frame,text="Please enter your first name").grid(row=1,column=0)
+		Label(frame,text="Please enter your first name:").grid(row=1,column=0)
 		self.firstNameBox = Entry(frame)
 		self.firstNameBox.grid(row=2,column=0)
 
 		# add entry box for last name
-		Label(frame,text="Please enter your last name").grid(row=3,column=0)
+		Label(frame,text="Please enter your last name:").grid(row=3,column=0)
 		self.lastNameBox = Entry(frame)
 		self.lastNameBox.grid(row=4,column=0)
 
+
+		# START: qualitative textbox entry questions
 		# add entry box for Cleaness
-		Label(frame,text="On a scale of 1 to 5, how clean are you? \n(1= not clean, 5 = very clean): ").grid(row=5,column=0)
+		Label(frame,text="On a scale of 1 to 5, how clean are you? \n(1= I can't see the floor.  5 = OCD) ").grid(row=5,column=0)
 		self.cleanBox = Entry(frame)
 		self.cleanBox.grid(row=6,column=0)
 
 		# add entry box for guests
-		Label(frame,text="On a scale of 1 to 5, how comfortable are you with guests in the room? \n(1 = not comfortable, 5 = very comfortable): ").grid(row=7,column=0)
+		Label(frame,text="On a scale of 1 to 5, how comfortable are you with guests in the room? \n(1 = People? No thanks.  5 = Partayy!) ").grid(row=7,column=0)
 		self.guestBox = Entry(frame)
 		self.guestBox.grid(row=8,column=0)
 
 		# add entry box for sound
-		Label(frame,text="On a scale of 1 to 5, how comfortable with noise are you?\n(1 = not comfortable, 5 = very comfortable): ").grid(row=9,column=0)
+		Label(frame,text="On a scale of 1 to 5, how comfortable with noise are you?\n(1 = It's better if we just don't talk.  5 = I'm pretty much in a band.").grid(row=9,column=0)
 		self.soundBox = Entry(frame)
 		self.soundBox.grid(row=10,column=0)
 
+
+		# START: radiobutton questions
 		# add entry box for smoke
-		Label(frame,text="Are you okay with somking? \n(Y or N):").grid(row=11,column=0)
+		Label(frame,text="Are you okay with smoking?").grid(row=11,column=0)
 
 		self.smokeVal = StringVar()
 		self.smokeVal.set("N/A")
@@ -69,7 +72,7 @@ class GUI():
 		self.smokeValNo.grid(row=13,column=0)
 
 		# add entry box for drinking
-		Label(frame,text="Are you okay with drinking? \n(Y or N):").grid(row=14,column=0)
+		Label(frame,text="Are you okay with drinking?").grid(row=14,column=0)
 
 		self.drinkVal = StringVar()
 		self.drinkVal.set("N/A")
@@ -80,7 +83,7 @@ class GUI():
 		self.drinkValNo.grid(row=16,column=0)
 
 		# add entry box for study habits
-		Label(frame,text="Do you study at home? \n(Y or N):").grid(row=17,column=0)
+		Label(frame,text="Do you study at home?").grid(row=17,column=0)
 
 		self.studyLocVal = StringVar()
 		self.studyLocVal.set("N/A")
@@ -91,7 +94,7 @@ class GUI():
 		self.studyLocValNo.grid(row=19,column=0)
 
 		# add entry box for gender
-		Label(frame,text="Do you identify as  male or female or N/A? \n(M, F): ").grid(row=20,column=0)
+		Label(frame,text="Do you identify as  male or female?").grid(row=20,column=0)
 
 		self.genderVal = StringVar()
 		self.genderVal.set("N/A")
@@ -102,7 +105,7 @@ class GUI():
 		self.genderValMale.grid(row=22,column=0)
 
 		# add entry box for gender preference
-		Label(frame,text="Do you prefer to room with a male or female? \n(M, F): ").grid(row=23,column=0)
+		Label(frame,text="Do you prefer to room with a male or female?").grid(row=23,column=0)
 
 		self.genderPrefVal = StringVar()
 		self.genderPrefVal.set("N/A")
@@ -111,6 +114,9 @@ class GUI():
 		self.genderPrefValFemale.grid(row=24,column=0)
 		self.genderPrefValMale = Radiobutton(frame, text ="Male", variable = self.genderPrefVal, value = 'M')
 		self.genderPrefValMale.grid(row=25,column=0)
+
+
+		# START: quantitative textbox questions
 
 		# add entry box for studyHours
 		Label(frame,text="\nOn average, how many hours do you study a week \n(1 = 0-2 hrs, 2 = 3-6 hrs, 3 = 7+ hrs): ").grid(row=26,column=0)
@@ -141,12 +147,11 @@ class GUI():
 		self.wakeBox.grid(row=35,column=0)
 
 
-		b = Button(frame, text="Submit", width=10, command = self.callback1)
-		b.grid(row=36,column=0)
+		b = Button(frame, text="Submit", width=10, command = self.callback)
+		b.grid(row=36,column=0, pady = 20)
 
 
-	# append the results to the people database and kill the GUI
-	def callback1(self):
+	def callback(self):
 
 		# create a list of each piece of data for easier error checking,
 		# also so that the data is organized according to the format read by sortRoommates.py
@@ -154,34 +159,91 @@ class GUI():
 		self.studyLocVal.get(), self.studyHrsBox.get(), self.tvHrsBox.get(), self.allNightBox.get(), self.genderVal.get(), self.genderPrefVal.get(), self.cleanBox.get(), self.soundBox.get(), \
 		self.guestBox.get()]
 
+		# the following lists are for error checking when creating the unique error statement below
+		nameIndicies = [0, 1]
+		relativeRangeIndicies = [2, 3, 7, 8, 9]
+		discreteIndicies = [12, 13, 14]
+
 		# initialize 3 different error variables
 		spaceInBox = False
 		emptyBox = False
 		unselectedRadioButton = False
+		nameError = False
+		numNotInRangeError = False
+		textBoxNumericError = False
 
 		# ensure each piece of data conforms to appropriate format
-		for data in dataList:			
-			if data == '':
+		for idx in range (len(dataList)):	
+			# check if a text box is empty		
+			if dataList[idx] == '':
 				emptyBox = True
 				break
-			if ' ' in data:
+
+			# check if there is a space in a text box
+			if ' ' in dataList[idx]:
 				spaceInBox = True
 				break
-			if data == "N/A":
+
+			# check if one of the radiobutton questions returned an invalid response
+			if dataList[idx] == "N/A":
 				unselectedRadioButton = True
 				break
+
+			# check that the name entry boxes contain only letters
+			if idx in nameIndicies:
+				if not dataList[idx].isalpha():
+					nameError = True
+					break
+
+			# check that the text entry boxes are numeric and within the specified range
+			if idx in discreteIndicies:
+				val = (dataList[idx])
+				if val.isnumeric():
+					val = int(val)
+					if val > 0 and val < 6:
+						continue
+					else:
+						numNotInRangeError = True
+						break
+				else:
+					textBoxNumericError = True
+					break
+			if idx in relativeRangeIndicies:
+				val = (dataList[idx])
+				if val.isnumeric():
+					val = int(val)
+					if val > 0 and val < 4:
+						continue
+					else:
+						numNotInRangeError = True
+						break
+				else:
+					textBoxNumericError = True
+					break
+
 
 		# initialize the error string
 		errorString = "One or more of your inputs is invalid."
 
 		# check if any of the errors occured, and customize and return the error message
-		if spaceInBox or emptyBox or unselectedRadioButton:
+		if spaceInBox or emptyBox or unselectedRadioButton or nameError or textBoxNumericError or numNotInRangeError:
 			if spaceInBox:
 				errorString += " Make sure there aren't any spaces in the text fields."
+
 			if emptyBox:
 				errorString += " Make sure you've filled in each input field."
+
 			if unselectedRadioButton:
 				errorString += " Double check that a button is selected in each field."
+
+			if nameError:
+				errorString += " Only enter letters into the name entry boxes."
+
+			if textBoxNumericError:
+				errorString += " Ensure that you only entered integers in the text boxes."
+
+			if numNotInRangeError:
+				errorString += " Be sure to only enter integers within the specified range."
 
 			# figure out how to display popups, for now just print the error
 			# message.showerror("Error", errorString)
@@ -189,19 +251,17 @@ class GUI():
 
 		# if no errors occurred, add the user to the database and kill the program
 		else:
-			result = '\n'
+			result = ''
 			for data in dataList:
 				result += data + ' '
 			result = result[:-1]
 
-			print (result)
-
-			infile = open("Roommates.txt","a")
+			infile = open("newUserTemp.txt","w")
 			infile.write(result)
 			infile.close()
 
 			self.master.destroy()
 
-
+	# configures scrollbar
 	def myfunction(self, event):
-		self.canvas.configure(scrollregion=self.canvas.bbox("all"),width=500,height=500)
+		self.canvas.configure(scrollregion=self.canvas.bbox("all"),width=400,height=500)
--- sortRoommates.py
+++ sortRoommates.py
@@ -7,11 +7,11 @@ import tkinter
 # this class represents each person's traits and preferences
 class Person ():
 
-	def __init__(self, id, firstName, lastName, wakingTime, bedtime, smoker, \
-	gender, preferredRoommateGender, cleanliness, guestComfort, loudness):
+	def __init__(self, id, firstName, lastName, wakingTime, bedtime, smoker, drinker, \
+	studyLoc, studyHrs, tvHrs, allNight, gender, preferredRoommateGender, cleanliness, guestComfort, loudness):
 		self.firstName = firstName
 		self.lastName = lastName
-		self.id = id
+		self.id = int(id)
 		self.gender = gender
 		self.preferredRoommateGender = preferredRoommateGender
 		self.wakingTime = int(wakingTime)
@@ -19,12 +19,20 @@ class Person ():
 		self.cleanliness = int(cleanliness)
 		self.guestComfort = int(guestComfort)
 		self.loudness = int(loudness)
-		self.smoker = (smoker)
-
-		# self.score = 0
+		self.smoker = smoker
+		self.drinker = drinker
+		self.studyLoc = studyLoc
+		self.studyHrs = int(studyHrs)
+		self.tvHrs = int(tvHrs)
+		self.allNight = int(allNight)
 
 	def __str__(self):
-		return self.firstName + " " + self.lastName
+		return (self.firstName + " " + self.lastName)
+
+	def dataReturn (self):
+		return (str(self.id) + " " + self.firstName + " " + self.lastName + " " + str(self.wakingTime) + " " + str(self.bedtime) + " " + self.smoker + " " + self.drinker + " " + \
+		self.studyLoc + " " + str(self.studyHrs) + " " + str(self.tvHrs) + " " + str(self.allNight) + " " + self.gender + " " + self.preferredRoommateGender + " " + str(self.cleanliness) + " " + \
+		str(self.guestComfort) + " " + str(self.loudness))
 
 # this class creates an object that holds all the people in a list
 class People ():
@@ -46,7 +54,9 @@ class People ():
 		for line in infile:
 			newPersonData = line.split()
 			self.addPerson(Person(newPersonData[0],newPersonData[1],newPersonData[2],newPersonData[3],newPersonData[4],newPersonData[5], \
-				newPersonData[6], newPersonData[7],newPersonData[8],newPersonData[9], newPersonData[10]))
+				newPersonData[6], newPersonData[7],newPersonData[8],newPersonData[9], newPersonData[10], newPersonData[11], newPersonData[12], \
+				newPersonData[13], newPersonData[14], newPersonData[15]))
+
 
 	def size(self):
 		return len(self.people)
@@ -64,8 +74,18 @@ class People ():
 			returnstr += str(person) + '\n'
 		return returnstr
 
+	def createNewUser(self, currentID):
+		newUserInfile = open("newUserTemp.txt","r")
+		newUserData = newUserInfile.readline().split()
+		newUser = Person(currentID, newUserData[0], newUserData[1],newUserData[2],newUserData[3],newUserData[4],newUserData[5], \
+				newUserData[6], newUserData[7],newUserData[8],newUserData[9], newUserData[10], newUserData[11], newUserData[12], \
+				newUserData[13], newUserData[14])
+		return newUser
+
 	# conducts survey and finds matches for user, then adds user to database if desired
-	def addNewUser (self):
+	# NOTE: this was the originial method by which we conducted the survey, however it is
+	# now conducted via the GUI
+	def testAddNewUser (self):
 		yesCheck = ["Y", "y", "Yes", "yes", "YES"]
 		noCheck = ["N", "n", "No", "no", "NO"]
 		maleGenderCheck = ["M","m","Male","male","MALE"]
@@ -138,31 +158,40 @@ class People ():
 		# initialize the person object for the new user
 		newUser = Person(currentID, user_firstName, user_lastName, user_wakingTime, user_bedtime, user_smoker, user_gender, user_preferredRoommateGender, user_cleanliness, user_guestComfort, user_loudness)
 
+	def searchForMatches(self, newUser):
+		# use these to check user input
+		yesCheck = ["Y", "y", "Yes", "yes", "YES"]
+		noCheck = ["N", "n", "No", "no", "NO"]
+
 		# parse the roommate database for matches
 		roommateScores = {}
 		for person in self.people:
 			# filter results for smoking and gender preferences of user and potential roommate
-			if person.smoker == newUser.smoker and person.gender == newUser.preferredRoommateGender and person.preferredRoommateGender == newUser.gender:
+			if person.smoker == newUser.smoker and person.gender == newUser.preferredRoommateGender and person.preferredRoommateGender == newUser.gender and newUser.drinker == person.drinker:
 				roommateScores[person] = math.sqrt(pow(newUser.wakingTime - person.wakingTime, 2) + pow(newUser.bedtime - person.bedtime, 2) + \
-					pow(newUser.cleanliness - person.cleanliness, 2) + pow(newUser.guestComfort - person.guestComfort, 2) + pow(newUser.loudness - person.loudness, 2))
+					pow(newUser.cleanliness - person.cleanliness, 2) + pow(newUser.guestComfort - person.guestComfort, 2) + \
+					pow(newUser.loudness - person.loudness, 2) + pow(newUser.studyHrs - person.studyHrs, 2) + \
+					pow(newUser.tvHrs - person.tvHrs, 2) + pow(newUser.allNight - person.allNight, 2))
 				
 		sorted_roommates = sorted(roommateScores.items(), key = operator.itemgetter(1))
 		matches = sorted_roommates[:10]
 				
 		# print out matches
-		print ('\nWe found '+str(len(matches)) + ' roommate(s) matching your preferences in our database:')
+		print ('\n' + str(newUser) + ', we found '+str(len(matches)) + ' roommate(s) matching your preferences in our database:')
+		print ()
+
+		percentMatchModifier = 12.126781251816649
 		for i in range(len(matches)):
-			print("NAME:", matches[i][0], "\tSCORE:", matches[i][1])
+			print("Name: " + '{:25s}'.format(str(matches[i][0])) + "Percent Match: " +  '{:2.2f}'.format(100 - (percentMatchModifier * matches[i][1])) + '%')
 
 		# prompt user whether or not they would like to be added to the database
-		userJoinDatabase = str(input("Would you like to add yourself to the roommate database? \n(Y or N): "))
+		userJoinDatabase = str(input("\nWould you like to add yourself to the roommate database? \n(Y or N): "))
 		if userJoinDatabase in yesCheck:
-			roommateDatabase = open("roommates.txt", "a")
-			newUserData = (str(newUser.id) + ' ' + newUser.firstName + ' ' + newUser.lastName + ' ' + str(newUser.wakingTime) + ' ' + str(newUser.bedtime) \
-			+ ' ' + newUser.smoker + ' ' + newUser.gender + ' ' + newUser.preferredRoommateGender + ' ' + str(newUser.cleanliness) + ' ' + str(newUser.guestComfort) + ' ' + str(newUser.loudness) + '\n')
-			roommateDatabase.write(newUserData)
+			roommateDatabase = open("Roommates.txt", "a")
+			roommateDatabase.write(newUser.dataReturn() + '\n')
 			roommateDatabase.close()
 			self.addPerson(newUser)
+			print ("\nYou have been successfully added to the database.")
 
 			# close the database file
 			roommateDatabase.close()
@@ -178,6 +207,9 @@ def main():
 		infile = open("Roommates.txt", "r")
 		roommateList.populatePeople(infile)
 
+		# initialize the current id number
+		currentID = (roommateList.people[-1].id) + 1
+
 		# use these to check user input
 		yesCheck = ["Y", "y", "Yes", "yes", "YES"]
 		noCheck = ["N", "n", "No", "no", "NO"]
@@ -185,11 +217,8 @@ def main():
 
 		# initialize flags to keep program continuous
 		end = False
-		run = False
 
-		# run a while loop that allows continous searches through the database
-		# unless user ends program
-		
+		# prompt user to search 
 		run = str(input('Would you like to search for roommate matches? \n(Y or N): '))
 		while run not in yesCheck and run not in noCheck:
 			print ('\nInput not recognized.')
@@ -198,6 +227,13 @@ def main():
 			root = tkinter.Tk()
 			displayGUI = GUI(root)
 			root.mainloop()
+
+			# grab the new user as a Person object
+			newUser = roommateList.createNewUser(currentID)
+			currentID += 1
+			# search for matches
+			roommateList.searchForMatches(newUser)
+
 		else:
 			end = True
 
@@ -205,7 +241,7 @@ def main():
 			endCheck = str(input('\nWould you like to search for roommate matches for another user? \n(Y or N): '))
 			while endCheck not in yesCheck and endCheck not in noCheck:
 				print ('\nInput not recognized.')
-				endCheck = str(input('\nWould you like to search for more matches for another user? \n(Y or N): '))
+				endCheck = str(input('\nWould you like to search for roommate matches for another user? \n(Y or N): '))
 			if endCheck in noCheck:
 				end = True
 			else:
@@ -213,6 +249,11 @@ def main():
 				displayGUI = GUI(root)
 				root.mainloop()
 
+				# grab the new user as a Person object
+				newUser = roommateList.createNewUser(currentID)
+				currentID += 1
+				# search for matches
+				roommateList.searchForMatches(newUser)
 
 		# close the text in-file
 		infile.close()
@@ -233,7 +274,7 @@ class TestSortRoomates(unittest.TestCase):
 		self.assertNotEqual(self.testDatabase, unpopulatedDatabase)
 
 	# test the creation of a Person
-	testPerson = Person(5000, 'Nikola', 'Tesla', 3, 3, 'N', 'M', 'M', 2, 2, 2)
+	testPerson = Person(9001, 'Nikola', 'Tesla', 3, 3, 'N', 'N', 'Y', 2, 2, 2, 'M', 'M', 3, 1, 2)
 	def testCreatePerson(self):
 		assert self.testPerson is not None
 
@@ -258,6 +299,10 @@ class TestSortRoomates(unittest.TestCase):
 		for i in range (len(self.testDatabase.people)):
 			peopleDatabaseStr += self.testDatabase.people[i].firstName + ' ' + self.testDatabase.people[i].lastName +'\n'
 		self.assertEqual(peopleDatabaseStr, str(self.testDatabase))
+
+	def testPersonDataReturn (self):
+		self.assertEqual(self.testPerson.dataReturn(), '9001 Nikola Tesla 3 3 N N Y 2 2 2 M M 3 1 2')
+
 
 if __name__ == '__main__':
 	unittest.main()
